b4d5f02e308ddbf9137b1ff3b22aef16e05c3e5f
somin <salth0601@gmail.com>
2023-12-13 17:15:04 +0900

    feature: 재시작 반복 로직 구현

--- src/main/kotlin/bridge/controller/GameController.kt
+++ src/main/kotlin/bridge/controller/GameController.kt
@@ -11,26 +11,50 @@ class GameController(
 ) {
 
     private val bridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
+    private val bridgeGame = BridgeGame()
+    private var isRestartable = false
 
     fun start() {
         outputView.printStartMessage()
         outputView.printInputBridgeSizeMessage()
         val bridgeSize = inputView.readBridgeSize()
+        playRound(bridgeSize)
+    }
+
+    private fun playRound(bridgeSize: Int) {
         do {
             val bridgeAnswers = bridgeMaker.makeBridge(bridgeSize)
-            play(bridgeAnswers)
-        } while (isRestartable())
+            playTern(bridgeAnswers)
+        } while (isRestartable)
     }
 
-    private fun play(bridgeAnswers: List<String>) {
+    private fun playTern(bridgeAnswers: List<String>) {
+        var resultString = listOf<String>()
         val moveHistory = listOf<MutableList<String>>(mutableListOf(), mutableListOf()) // 빈 다리 문자열 생성
         for (answer in bridgeAnswers) { // 다리 사이즈만큼 턴 반복
-            outputView.printChoiceMovingPlace() // 이동 칸 안내 메시지 출력
-            val input = inputView.readMoving() // 이동 칸 입력 받기
-            val result = BridgeGame().move(moveHistory, answer, input) // 입력과 정답을 바탕으로 이동 진행
-            outputView.printMap(result)
+            val resultPair = proceedRound(moveHistory, answer)
+            resultString = resultPair.second
+            if (!resultPair.first) {
+                isRestartable = checkRetry()
+                break
+            }
         }
+        outputView.printMap(resultString)
     }
 
-    private fun isRestartable() = false
+    private fun proceedRound(moveHistory: List<MutableList<String>>, answer: String): Pair<Boolean, List<String>> {
+        outputView.printChoiceMovingPlace() // 이동 칸 안내 메시지 출력
+        val input = inputView.readMoving() // 이동 칸 입력 받기
+        val resultString = bridgeGame.move(moveHistory, answer, input) // 입력과 정답을 바탕으로 이동 진행
+        outputView.printMap(resultString)
+        return Pair((answer == input), resultString)
+    }
+
+    private fun checkRetry(): Boolean {
+        outputView.printRetry()
+//            inputView. // 입력도 받아야 함
+        val isRetry = true
+        return isRetry
+    }
+
 }

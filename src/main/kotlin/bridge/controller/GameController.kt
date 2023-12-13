bbd6fda0aede1ba3834531d06e075c59223e60dd
somin <salth0601@gmail.com>
2023-12-13 16:35:13 +0900

    refactor: 정답 문자열 생성 메서드 BridgeGame으로 이동

--- src/main/kotlin/bridge/controller/GameController.kt
+++ src/main/kotlin/bridge/controller/GameController.kt
@@ -23,27 +23,13 @@ class GameController(
     }
 
     private fun play(bridgeAnswers: List<String>) {
-        val bridgeString = listOf<MutableList<String>>(mutableListOf(), mutableListOf()) // 빈 다리 문자열 생성
+        val moveHistory = listOf<MutableList<String>>(mutableListOf(), mutableListOf()) // 빈 다리 문자열 생성
         for (answer in bridgeAnswers) { // 다리 사이즈만큼 턴 반복
-            outputView.printChoiceMovingPlace() // 이동 칸 안내 문자
+            outputView.printChoiceMovingPlace() // 이동 칸 안내 메시지 출력
             val input = inputView.readMoving() // 이동 칸 입력 받기
-            val (upperResult, lowerResult) = BridgeGame().move(answer, input) // 입력값 바탕으로 결과 받아오기
-            bridgeString.add(upperResult, lowerResult) // 다리 문자열에 추가
-            makeTurnResult(bridgeString) // 다리 문자열 기반으로 결과 생성
+            val result = BridgeGame().move(moveHistory, answer, input) // 입력과 정답을 바탕으로 이동 진행
+            outputView.printMap(result)
         }
-    }
-
-    private fun makeTurnResult(bridgeString: List<MutableList<String>>) {
-        val bridgeResultString = bridgeString.map { it.joinToString(separator = " | ", prefix = "[ ", postfix = " ]") }
-        bridgeResultString.forEach { bridge ->
-            bridge.forEach { print(it) }
-            println()
-        }
-    }
-
-    private fun List<MutableList<String>>.add(s1: String, s2: String) {
-        this[0].add(s1)
-        this[1].add(s2)
     }
 
     private fun isRestartable() = false
--- src/main/kotlin/bridge/model/BridgeGame.kt
+++ src/main/kotlin/bridge/model/BridgeGame.kt
@@ -5,12 +5,27 @@ package bridge.model
  */
 class BridgeGame {
 
+    fun move(bridgeString: List<MutableList<String>>, answer: String, input: String): List<String> {
+        val (upperResult, lowerResult) = BridgeGame().getResult(answer, input) // 입력값 바탕으로 결과 받아오기
+        bridgeString.add(upperResult, lowerResult) // 다리 문자열에 추가
+        return makeTurnResult(bridgeString) // 다리 문자열 기반으로 결과 생성
+    }
+
+
+    private fun List<MutableList<String>>.add(s1: String, s2: String) {
+        this[0].add(s1)
+        this[1].add(s2)
+    }
+
+    private fun makeTurnResult(bridgeString: List<MutableList<String>>): List<String> {
+        return bridgeString.map { it.joinToString(separator = " | ", prefix = "[ ", postfix = " ]") }
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-
-    fun move(answer: String, input: String): List<String> {
+    fun getResult(answer: String, input: String): List<String> {
         val result = when (answer == input) {
             true -> "O"
             false -> "X"
